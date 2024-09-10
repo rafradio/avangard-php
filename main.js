8b8429569340d358f33a82d65e2fb5395ab31e04
Rafail Abdyushev <rafaelab@mail.ru>
2024-09-10 20:40:35 +0300

    main.js

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
 let urlButton = document.getElementById("URL");
+let resultButton = document.getElementById("Copy");
+
 urlButton.onclick = async () => {
     let url = new URL(window.location.href);
     url.pathname = "/AvangardPHP/request.php";
-    console.log("from url button = ", url.toString());
     await makeRequest(url);
-//    const result = await makeRequest(url);
-//    console.log("url result = ", result);
 }
 
 const makeRequest = async (url) => {
-    let dataToSend = {'data': 'some data'};
+    let dataToSend = {'data': document.getElementById("Uname").value};
     const request = new Request(url, {
                                 method: "POST",
                                 headers: {
                                             'Content-Type': 'application/json;charset=utf-8',
-
                                         },
                                 body: JSON.stringify(dataToSend)
                                 });
     try {
         const response = await fetch(request);  
         if (!response.ok) {
-            throw new Error(response.statusText);
+            throw new Error(`Response status: ${response.status}`);
         }
         const data = await response.json();
-        console.log("url result = ", data);
-
+        console.log("url result = ", data.data);
+        document.getElementById("UResult").value = data.data;
+        document.getElementById("Uname").value = "";
     }
     catch(error) {
-        console.log(error);
+        console.log(error.message);
     }
     
-//    return data;
+}
+
+resultButton.onclick = () => {
+  let copyText = document.getElementById("UResult");
+  copyText.select();
+  navigator.clipboard.writeText(copyText.value);
 }
 
