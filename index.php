8156263f8dd171f327667c7d90405f576b1e3a6f
Rafail Abdyushev <rafaelab@mail.ru>
2024-09-10 07:25:18 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Short url</title>
+        <link href="styles.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <?php
+            
+        ?>
+        <div class="container">
+            <fieldset class="flex-fieldset">
+                <legend>Введите URL</legend>
+                <div class="elements">                   
+                    <label for="Uname">Введите URL:</label>
+                    <input type="text" id="Uname" name="Uname">
+                    <button id="URL">Submit</button>
+                </div>
+                <div class="elements">                   
+                    <label for="UResult">Ваш результат:</label>
+                    <input type="text" id="UResult" name="UResult">
+                    <button id="Copy">Copy</button>
+                </div>
+            </fieldset>
+        </div>
+        <script src="main.js"></script>
+    </body>
+</html>
