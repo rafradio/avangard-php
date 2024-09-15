4d2d3c1c6785cfbbf8fb353f6667a866e447d3d4
Rafail Abdyushev <rafaelab@mail.ru>
2024-09-15 10:47:19 +0300

    request.php

--- request.php
+++ request.php
@@ -2,6 +2,10 @@
     include 'Functions.php';
     $bodyFromFront = file_get_contents('php://input');
     $data = json_decode($bodyFromFront);
+    
+    // здесь формируется url который направляется клиенту
     $resultUrl = createURL($data->{'data'});
+    
     $out['data'] = $resultUrl;
+    
     echo json_encode($out);
